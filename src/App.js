b540af18b4c758c1e3aa491f4f0a939c90572609
qq8721443 <qq8721443@naver.com>
2020-12-30 20:51:42 +0900

    add modal

--- src/App.js
+++ src/App.js
@@ -10,15 +10,14 @@ function App() {
   const [can, setCan] = React.useState(true)
 
   const popUpCard = () => {
-    
     document.getElementById('itembox').style.top = '0%';
-    document.getElementById('popupbtn').innerText = '새해소망 제출하기'
+    document.getElementById('popupbtn').firstChild.innerText = '새해소망 제출하기'
     setBtnState(0)
   }
 
   const sendCard = () => {
     document.getElementById('itembox').style.left = '100%';
-    document.getElementById('popupbtn').innerText = '새해소망 제출완료'
+    document.getElementById('popupbtn').firstChild.innerText = '새해소망 제출완료'
     alert(`${username} ${email} ${content}`)
     setBtnState(1)
     setCan(false)
@@ -26,8 +25,36 @@ function App() {
 
   const closeCard = () => {
     document.getElementById('itembox').style.top = '100%';
-    document.getElementById('popupbtn').innerText = '새해소망 작성하기'
+    document.getElementById('popupbtn').firstChild.innerText = '새해소망 작성하기'
     setBtnState(1)
+  }
+
+  const returnHope = () => {
+    console.log(document.getElementsByClassName('hopeitem').length)
+  }
+
+  const addHope = () => {
+    const addingHope = document.createElement('div')
+    const parent = document.getElementById('usercont')
+    addingHope.className = 'hopeitem'
+    addingHope.innerText = 'added hope'
+    parent.appendChild(addingHope)
+  }
+  
+  const openModal = () => {
+    const modal = document.getElementById('modal')
+    const modalback = document.getElementById('modalback')
+
+    modal.classList.remove('hidden')
+    modalback.classList.remove('hidden')
+  }
+
+  const removeModal = () => {
+    const modal = document.getElementById('modal')
+    const modalback = document.getElementById('modalback')
+
+    modal.classList.add('hidden')
+    modalback.classList.add('hidden')
   }
 
   return (
@@ -37,30 +64,64 @@ function App() {
           test
         </div>
       </div>
+      <div style={{textAlign:'center', fontSize:22, paddingTop:15}}>
+        새해 목표를 정해보세요
+      </div>
       <div id='content'>
         <div id='mainCont'>
-          <div class='mainContItem'>
-            1
+          <div className='mainContItem'>
+            <div className='listitem'>
+              test1
+            </div>
+            <div className='listitem'>
+              test2
+            </div>
+            <div className='listitem'>
+              test3
+            </div>
+            <div className='listitem'>
+              test4
+            </div>
+            <div className='listitem'>
+              test5
+            </div>
+            <div className='listitem'>
+              test6
+            </div>
+            <div className='listitem'>
+              test7
+            </div>
           </div>
-          <div class='mainContItem'>
-            2
-          </div>
-          <div class='mainContItem'>
-            3
+          <div className='mainContItem'>
+            <div className='boardlist'>
+              test1
+            </div>
+            <div className='boardlist'>
+              test2
+            </div>
           </div>
         </div>
         <div id='itembox'>
           <div id='user'>
-            <input type='text' placeholder='nickname' onChange = {e => setUsername(e.target.value)}/>
-            <input type='button' value='test' onClick={() => alert(username)}/>
-            <input type='text' placeholder='email' onChange={e => setEmail(e.target.value)}/>
-            <label><input type='checkbox' value='0'/>private option</label>
+            <input type='text' tabIndex='-1' placeholder='nickname' onChange = {e => setUsername(e.target.value)}/>
+            <input type='button' tabIndex='-1' value='test' onClick={() => alert(username)}/>
+            <input type='text' tabIndex='-1' placeholder='email' onChange={e => setEmail(e.target.value)}/>
+            <label><input type='checkbox' tabIndex='-1' value='0'/>private option</label>
             <div>
-              <textarea onChange={e => setContent(e.target.value)} style={{all:'unset', width:'97%', height:'100%', padding:'5px', backgroundColor:'white'}}></textarea>
+              <textarea tabIndex='-1' onChange={e => setContent(e.target.value)} style={{all:'unset', width:'97%', height:'100%', padding:'5px', backgroundColor:'white'}}></textarea>
             </div>
           </div>
           <div id='usercont'>
-            hope list
+            <input type='button' value='add hope' onClick={() => openModal()}/>
+            <div className='hopeitem'>
+              hope1
+            </div>
+            <div className='hopeitem'>
+              hope2
+            </div>
+            <div className='hopeitem'>
+              hope3
+            </div>
           </div>
           <div id='closebtn' onClick={() => closeCard()}>
             <span style={{position:'relative', width:10, height:10, marginTop:10}}>X</span>
@@ -69,7 +130,15 @@ function App() {
         <div id='popupbtn' onClick={
           (can?btnState?() => popUpCard():() => sendCard():undefined)
         }>
-          새해소망 작성하기
+          <p>새해소망 작성하기</p>
+        </div>
+      </div>
+      <div id='modalback' class='hidden'>
+      </div>
+      <div id='modal' class='hidden'>
+        test
+        <div class='btn-normal' onClick={() => removeModal()}>
+          <p>취소</p>
         </div>
       </div>
     </div>
